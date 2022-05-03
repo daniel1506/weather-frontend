959152f7292a28d920bcffadfdfbe8fb2bd81c2d
Man Ho <kodyglock@gmail.com>
2022-05-04 01:10:03 +0800

    change to true routing, instead of fake general context routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import { React, useContext } from "react";
 import AuthContext from "./store/auth-context";
 import Auth from "./Auth";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate, useMatch } from "react-router-dom";
 import { Box, CssBaseline } from "@mui/material";
 import Homepage from "./pages/Homepage/Homepage";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import TableView from "./pages/Homepage/TableView";
+import MapView from "./pages/Homepage/MapView";
+import FavoriteView from "./pages/Homepage/FavoriteView";
+import AdminView from "./pages/Homepage/AdminView/AdminView";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import PrefContext from "./store/preference-context";
+import Navbar from "./components/Navbar";
 function App() {
   const prefCtx = useContext(PrefContext);
+  const authCtx = useContext(AuthContext);
   return (
     <ThemeProvider theme={prefCtx.theme}>
       <CssBaseline />
       <Router>
+        {true && <Navbar />}
         <Routes>
-          <Route path="/homepage" element={<Homepage />}></Route>
-          <Route path="*" element={<Auth />}></Route>
+          <Route path="/" element={<Auth />}></Route>
+          <Route path="/homepage" element={<TableView />}></Route>
+          <Route path="/mapview" element={<MapView />}></Route>
+          <Route path="/favoriteview" element={<FavoriteView />}></Route>
+          <Route path="/adminview" element={<AdminView />}></Route>
+          <Route path="*" element={<Navigate to="/homepage" />}></Route>
         </Routes>
       </Router>
     </ThemeProvider>
