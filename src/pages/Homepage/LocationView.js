25528aa3fc7da64dfb3e2f7defefb09b2de52600
Li Hong Man <kongman61@gmail.com>
2022-05-12 00:37:15 +0800

    modified a bit of the LocationView js

--- src/pages/Homepage/LocationView.js
+++ src/pages/Homepage/LocationView.js
@@ -2,7 +2,11 @@ import React from "react";
 import { useParams } from "react-router-dom";
 function LocationView() {
   const { cityName } = useParams();
-  return <div>LocationView, current cityName:{cityName}</div>;
+  return (
+    <div>
+      LocationView, current cityName:{cityName}
+    </div>
+  )
 }
 
 export default LocationView;
